4636884100d8983615f92a094946d3bc48f82d1a
Den T <tsyhanenko.dp@gmail.com>
2022-09-11 13:37:55 +0300

    Connect JS

---
+++ sctipts/cataloge.js
@@ -0,0 +1,21 @@
+const CATALOG = [
+    {
+        id: 'el1',
+        img: 'images/Decor Plate.jpg',
+        name: 'Decor Plate',
+        price: 65.00,
+    },
+    {
+        id: 'el2',
+        img: '',
+        name: 'Mint Pottery',
+        price: 75.00,
+    },
+    {
+        id: 'el3',
+        img: '',
+        name: 'Set Of Potterys',
+        price: 125.00,
+    },
+];
+

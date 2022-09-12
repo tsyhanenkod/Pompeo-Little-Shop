d25dd1f95fac4164f66a889587927249ee2bdc08
Den T <tsyhanenko.dp@gmail.com>
2022-09-12 16:11:35 +0300

    try fix js

--- sctipts/cataloge.js
+++ sctipts/cataloge.js
@@ -1,37 +1,37 @@
 const CATALOG = [
     {
         id: 'el1',
-        img: 'images/Decor Plate.JPG',
+        img: 'images/Decor Plate.jpg',
         name: 'Decor Plate',
         price: '$ 65.00 USD',
     },
     {
         id: 'el2',
-        img: 'images/Mint Pottery.JPG',
+        img: 'images/Mint Pottery.jpg',
         name: 'Mint Pottery',
         price: '$ 75.00 USD',
     },
     {
         id: 'el3',
-        img: 'images/Set Of Potterys.JPG',
+        img: 'images/Set Of Potterys.jpg',
         name: 'Set Of Potterys',
         price: '$ 125.00 USD',
     },
     {
         id: 'el4',
-        img: 'images/Orange Ceramic.JPG',
+        img: 'images/Orange Ceramic.jpg',
         name: 'Orange Ceramic',
         price: '$ 55.00 USD',
     },
     {
         id: 'el5',
-        img: 'images/Dark Bowl.JPG',
+        img: 'images/Dark Bowl.jpg',
         name: 'Dark Bowl',
         price: '$ 115.00 USD',
     },
     {
         id: 'el6',
-        img: 'images/Square Pottery.JPG',
+        img: 'images/Square Pottery.jpg',
         name: 'Square Pottery',
         price: '$ 75.00 USD',
     },
